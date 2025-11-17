c5c2dc9dda2b86709a46de16bdc8209dc6a37955
nayesimbana13 <naye.simbana13@gmail.com>
2025-11-17 12:21:16 -0500

    Add files via upload

---
+++ TAREAS/tarea1/MultiplicadorDeFracciones.cpp
@@ -0,0 +1,23 @@
+/***********************************************************************
+ * Module:  MultiplicadorDeFracciones.cpp
+ * Author:  Usuario
+ * Modified: martes, 21 de octubre de 2025 18:38:58
+ * Purpose: Implementation of the class MultiplicadorDeFracciones
+ ***********************************************************************/
+
+#include "Fraccion.h"
+#include "MultiplicadorDeFracciones.h"
+
+////////////////////////////////////////////////////////////////////////
+// Name:       MultiplicadorDeFracciones::multiplicar(Fraccion f1, Fraccion f2)
+// Purpose:    Implementation of MultiplicadorDeFracciones::multiplicar()
+// Parameters:
+// - f1
+// - f2
+// Return:     Fraccion
+////////////////////////////////////////////////////////////////////////
+
+Fraccion MultiplicadorDeFracciones::multiplicar(Fraccion f1, Fraccion f2)
+{
+   // TODO : implement
+}
